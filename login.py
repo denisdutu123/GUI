2a6673644167af2dc8168fe946e7bb0f5a859bd5
denisdutu123 <dutudenis42@gmail.com>
2025-06-21 17:34:52 +0100

    Update login.py

--- login.py
+++ login.py
@@ -4,6 +4,17 @@ screen = Tk()
 screen.title("Log In")
 screen.geometry("800x700")
 screen.config(background = "red")
+#function for saving detail
+def detail():
+    detuse = entry.get()
+    detpas = passw.get()
+    file = open("text.txt", 'a')
+    file.write(f"Username:{detuse}, password: {detpas}\n")
+    #clear entry box
+    entry.delete(0, END)
+    passw.delete(0, END)
+    
+    
 #label
 use = Label(screen, text = "Username:", background = "Grey", font = ("courgette", 30))
 use.place(x = 200, y = 200)
@@ -12,9 +23,9 @@ passwor.place(x = 200, y = 300)
 #entry
 entry = Entry(screen, background = "Grey", width = 20, font = ("courgette", 30))
 entry.place(x = 350, y = 200)
-passw = Entry(screen, background = "Grey", font = ("Grey", 30))
+passw = Entry(screen, show = "*", background = "Grey", font = ("Grey", 30))
 passw.place( x = 350, y = 300)
 #button
-submit = Button(screen, text = "Submit", background = "Grey", font = ("courgette", 30))
+submit = Button(screen, text = "Submit", background = "Grey", font = ("courgette", 30), command = detail)
 submit.place(x = 200, y = 375)
 screen.mainloop()
