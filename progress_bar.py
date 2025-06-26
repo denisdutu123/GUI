65cf26f88cf2672bb13bfa7d9d3318f812c7b9ae
denisdutu123 <dutudenis42@gmail.com>
2025-06-26 16:41:13 +0100

    Update progress_bar.py

--- progress_bar.py
+++ progress_bar.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.ttk import *
+import threading
 
 screen = Tk()
 screen.geometry("400x300")
@@ -13,24 +14,25 @@ def button():
     import time
     bar["value"] = 50
     screen.update_idletasks()
-    time.sleep(2)
+    time.sleep(1)
     bar["value"] = 100
     screen.update_idletasks()
-    time.sleep(2)
+    time.sleep(1)
     bar["value"] = 150
     screen.update_idletasks()
-    time.sleep(2)
+    time.sleep(1)
     bar["value"] = 200
     screen.update_idletasks()
-    time.sleep(2)
+    time.sleep(1)
     bar["value"] = 250
    
 
 bar = Progressbar(screen,style = "custom.Horizontal.TProgressbar", orient = HORIZONTAL, length = 250, mode = "determinate")
 bar.place( x = 75, y =150)
 
-start = Button(screen, width = 10, text = "Start", command = button)
-start.place(x = 75, y  = 225)
+#start = Button(screen, width = 10, text = "Start", command =lambda: 
+#start.place(x = 75, y  = 225)
+Button(screen, text = "Start", command = lambda: threading.Thread(target = button).start()).pack(pady = 10)
 
 
 screen.mainloop()
