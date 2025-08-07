e4acec04d69410b4dce8a38a3b68d58f138bad8b
denisdutu123 <dutudenis42@gmail.com>
2025-08-07 16:52:32 +0100

    Add files via upload

---
+++ list.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+screen = Tk()
+#screen.geometry("800x400")
+
+save = Button(screen, text = "Save", font = ("courgette", 15))
+save.pack(side = LEFT, padx = 5, pady = 5)
+
+ent = Entry(screen, width = 20)
+ent.pack(padx = 5, pady = 5)
+
+add = Button(screen, text = "Add", font = ("courgette", 15))
+add.pack(padx = 5, pady = 5)
+
+open = Button(screen, text = "Open", font = ("courgette", 15))
+open.pack(side = RIGHT, padx = 5, pady = 5)
+
+delete = Button(screen, text = "Delete", font = ("courgeette", 15))
+delete.pack(padx = 5, pady = 5)
+
+verti = Frame(screen)
+scroll = Scrollbar(verti)
+scroll.pack(side = RIGHT, fill = Y)
+list = Listbox(verti, width = 40, height = 40, yscrollcommand = scroll.set, bg = "orange")
+
+for i in range(1, 50):
+    list.insert(END, "Item" + str(i))
+list.pack(side = LEFT, padx = 5)
+scroll.config(command = list.yview)
+verti.pack(side = RIGHT, padx = 5)
+
+
+
+
+
+
+
+
+
+
+
+
+screen.mainloop()
