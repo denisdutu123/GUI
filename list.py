812d7a5023558d9d523408f5ff2a87b4b84d85e9
denisdutu123 <dutudenis42@gmail.com>
2025-08-07 16:56:55 +0100

    Update list.py

--- list.py
+++ list.py
@@ -6,22 +6,24 @@ screen = Tk()
 save = Button(screen, text = "Save", font = ("courgette", 15))
 save.pack(side = LEFT, padx = 5, pady = 5)
 
-ent = Entry(screen, width = 20)
-ent.pack(padx = 5, pady = 5)
+
 
 add = Button(screen, text = "Add", font = ("courgette", 15))
-add.pack(padx = 5, pady = 5)
+add.pack(side = RIGHT, padx = 5, pady = 5)
 
 open = Button(screen, text = "Open", font = ("courgette", 15))
-open.pack(side = RIGHT, padx = 5, pady = 5)
+open.pack( padx = 5, pady = 5)
 
 delete = Button(screen, text = "Delete", font = ("courgeette", 15))
 delete.pack(padx = 5, pady = 5)
 
+ent = Entry(screen, width = 20)
+ent.pack(padx = 5, pady = 5)
+
 verti = Frame(screen)
-scroll = Scrollbar(verti)
+scroll = Scrollbar(verti, orient = "vertical")
 scroll.pack(side = RIGHT, fill = Y)
-list = Listbox(verti, width = 40, height = 40, yscrollcommand = scroll.set, bg = "orange")
+list = Listbox(verti, width = 60, yscrollcommand = scroll.set, bg = "orange")
 
 for i in range(1, 50):
     list.insert(END, "Item" + str(i))
