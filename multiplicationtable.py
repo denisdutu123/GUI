1e7829f45139b77a67788a667372d32d1211d23b
denisdutu123 <dutudenis42@gmail.com>
2025-08-21 16:56:53 +0100

    Update multiplicationtable.py

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -4,14 +4,34 @@ from tkinter.ttk import *
 screen = Tk()
 screen.title("maths")
 
+def generate():
+    
+    s1 = ""
+    for i in range(1, numb1.get() +1):
+        s1 += str(value.get())+"X"+str(i)+ " = " + str(value.get()*i)+"\n"
+    display.config(text = s1)
+        
+        
+        
+        
+    
+
+
+
+display = Label(screen)
+display.grid(row = 4, column = 0)
+
 mt = Label(screen, text = "Mathematical Table")
 mt.grid(row = 0, column = 0, padx = (10, 10), columnspan = 3)
+
+stor = Button(screen, text = "generate", command = generate)
+stor.grid(row = 2, column = 0)
 
 nr = Label(screen, text = "Number and Range:")
 nr.grid(row = 1, column = 0)
 
 value = IntVar()
-num = Combobox(screen, textvariable = value)
+num = Combobox(screen, textvariable = value, width = 3)
 num["values" ] = tuple(range(50))
 num.grid(row = 1, column = 1)
 
