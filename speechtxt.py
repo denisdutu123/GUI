cde9d9a600ca9fd5f1e7f0a3c00d1a465f357e10
denisdutu123 <dutudenis42@gmail.com>
2025-10-02 16:49:03 +0100

    Update speechtxt.py

--- speechtxt.py
+++ speechtxt.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import os
+from tkinter import messagebox
 import speech_recognition as sr
 from tkinter import filedialog
 
@@ -13,11 +13,19 @@ def click():
         print("Speaking recognition started")
         aud = recogn.listen(source)
         try:
-            text = recogn.recognise_google(aud)
+            text = recogn.recognize_google(aud)
         except:
             text = "You are currently not speaking"
         output.delete(1.0, END)
         output.insert(END, text)
+
+def sav():
+    sa = filedialog.asksaveasfile(defaultextension = ".txt")
+    if sa:
+        sa.write(output.get(1.0, END))
+        sa.close()
+    else:
+        messagebox.showerror("Warning message", "text not saved")
 
 main = Label(screen, text = "Voice Notepad", font = ("courgette", 25), bg = "Blue")
 main.place(x = 250, y = 60)
@@ -28,7 +36,7 @@ clickonme.place(x = 20, y = 200)
 output = Text(screen, width = 20, height = 3, font = ("courgette", 20))
 output.place(x = 250, y = 200)
 
-save = Button(screen, text = "save the text", font = ("courgette", 20))
+save = Button(screen, text = "save the text", font = ("courgette", 20), command = sav)
 save.place(x = 530, y = 220)
 
   
